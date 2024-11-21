972fc0be5d59a64d109c35676af5e6effad1166e
ilievn1 <122593669+ilievn1@users.noreply.github.com>
2024-11-21 18:58:08 +0200

    Add experiments with varying params

---
+++ demo_four.py
@@ -0,0 +1,59 @@
+import numpy as np
+from .utils import *
+import BartlettBeamformer
+import CaponBeamformer
+import MUSIC
+
+# Scenario # | Description | Key Modifications
+# ----------------------------------------------
+# 3          | Unequal sig powers | 2 sources at -20° and 20°, SNR_1 is 15dB, SNR_1 is -15dB, AWGN
+
+M = 4
+d = 0.5 # in wavelengths
+N = 100  # sample size
+
+inc_ang_deg = [-20, 20]
+thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x K) Incident angles of test signal
+K = thetas_deg.shape[1] # K MUST BE < M - 1 FOR CORRECT DETECTION
+thetas_rad = np.deg2rad(thetas_deg)
+
+# Generate source signals
+soi = np.random.randn(K, N)   # Signal(s) of Interest
+
+# Augment generated signals with the given SNR
+snr = [15,-15]
+snr = np.asarray(snr) # (K,)
+power = 10**(snr / 10) 
+power = np.sqrt(power) 
+power = np.diag(power) # (K x K)
+
+soi = power @ soi # (K x K) @ (K x N)
+
+A = ula_steering_matrix(M,d,thetas_rad) # (M x K)
+
+soi_matrix = A @ soi # (M x K) @ (K x N) = (M x N)
+
+# Generate multichannel uncorrelated noise
+noise = np.random.randn(M,N) + 1j*np.random.randn(M,N)
+noise = 0.5 * noise # split between Re and Im components.
+
+# Create received signal
+tx_signal = soi_matrix + noise
+
+# R matrix calculation
+# outside lib methds to allow different ways of calculating and augmenting
+R = (tx_signal @ tx_signal.conj().T)/tx_signal.shape[1]
+
+# Generate steering vectors
+ula_st_vectors = ula_scan_steering_matrix(M,0.5,angular_resolution=1)
+
+# DOA estimation
+estimates,Bartlett_PAD = BartlettBeamformer.estimate_doa(R, ula_st_vectors)
+print("Bartlett_PAD estimates", estimates)
+estimates,Capon_PAD = CaponBeamformer.estimate_doa(R, ula_st_vectors)
+print("Capon_PAD estimates", estimates)
+estimates,MUSIC_ORTAD = MUSIC.estimate_doa(R, ula_st_vectors)
+print("MUSIC_ORTAD estimates", estimates)
+
+DOA_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC"])
+DOA_polar_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC"])
--- main.py
+++ main.py
@@ -2,29 +2,62 @@ import numpy as np
 from .utils import *
 import BartlettBeamformer
 import CaponBeamformer
-# ===============================================================================================
-# Simple case
-# Params: 
-# SNR = 1
-# sig_numb = 1
+import MUSIC
+# Scenarios for Testing DOA Estimation Performance
+# The following table summarizes different test cases designed to explore the behavior of DOA estimators
+# under various challenging conditions. Each scenario introduces a unique challenge by modifying
+# the signal characteristics or the environment.
 
-M = 16
+# Scenario # | Description | Key Modifications
+# ----------------------------------------------
+# 1          | Close angular spacing | 2 sources at 20° and 21°, SNR=20dB, AWGN
+# 2          | Low SNR/ Hign noise | 2 sources at -20° and 20°, SNR reduced to -40 dB, AWGN noise dominates signal,
+# 3          | High SNR/ Low noise | 2 sources at -20° and 20°, SNR reduced to 80 dB, AWGN 
+# 4          | Unequal sig powers | 2 sources at -20° and 20°, SNR_1 is 15dB, SNR_2 is -15dB, AWGN
+# 5          | Calibration errors | 2 sources at -20° and 20°, SNR_1 is 15dB, SNR_2 is -15dB, AWGN, steering vectors perturbed
+# 6          | Correlated sources | 2 sources at -20° and 20°, SNR_1 is 15dB, SNR_2 is -15dB, AWGN, highly correlated
+# 7          | Correlated noise | 2 sources at -20° and 20°, SNR_1 is 15dB, SNR_2 is -15dB, AWGN highly correlated
+# 8          | Off-grid resolution | 2 sources at -23.7° and 23.3°, SNR_1 is 15dB, SNR_2 is -15dB, AWGN, 1 deg angular scan resolution
+# 9          | Reduced number of samples | 20 samples instead of 100, 2 sources at -20° and 20°, SNR_1 is 15dB, SNR_2 is -15dB, AWGN
+# 10         | Multipath coherency | 1 source at -20°, SNR=20dB, reflection at 20°, AWGN
+# 11         | Sparse array | 2-element ULA, less spatial information, models struggle with resolution
+# 12         | Complex noise distributions | Noise with mixed Laplacian, Rayleigh, Gaussian, Exponential, etc., across channels
+
+# TODO: Add after establishing stochastic and deterministic signal models
+# 13         | Time-varying DOAs | DOAs changing sinusoidally over time, models must handle dynamic sources
+# 14         | Signals with harmonics | Primary signal with harmonics added, interferes with accurate estimation
+
+# Each scenario explores different aspects of DOA estimation, including challenges with:
+# - Low or high SNR
+# - Overlapping sources or noise
+# - Array geometry issues
+# - Complex noise distributions
+# - Multipath or harmonics
+
+M = 4
 d = 0.5 # in wavelengths
-N = 10000  # sample size
+N = 100  # sample size
 
-inc_ang_deg = [20]
-
-# Generate source signal
-soi = np.random.randn(1, N)   # Signal(s) of Interest
-   
-thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x P) Incident angles of test signal   
-P = thetas_deg.shape[1] # P MUST BE < M FOR CORRECT DETECTION
+inc_ang_deg = [20, 21]
+thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x K) Incident angles of test signal
+K = thetas_deg.shape[1] # K MUST BE < M - 1 FOR CORRECT DETECTION
 thetas_rad = np.deg2rad(thetas_deg)
 
-elems = np.arange(M).reshape(-1,1)
-a = np.exp(-1j * 2 * np.pi * d * elems @ np.sin(thetas_rad)) # (M x P)
+# Generate source signals
+soi = np.random.randn(K, N)   # Signal(s) of Interest
 
-soi_matrix = a @ soi # (M x P) @ (P x N) = (M x N)
+# Augment generated signals with the given SNR
+snr = [20,20]
+snr = np.asarray(snr) # (K,)
+power = 10**(snr / 10) 
+power = np.sqrt(power) 
+power = np.diag(power) # (K x K)
+
+soi = power @ soi # (K x K) @ (K x N)
+
+A = ula_steering_matrix(M,d,thetas_rad) # (M x K)
+
+soi_matrix = A @ soi # (M x K) @ (K x N) = (M x N)
 
 # Generate multichannel uncorrelated noise
 """
@@ -35,51 +68,56 @@ then "noise" var dimensions need be (num_sensors x num_samples).
 
 Noise prior to phase shifting samples:
 For AWGN, noise can be added to received signal prior to phase-shifting and act as environmental noise
-then "noise" var dimensions need be (num_singals x num_samples).
+then "noise" var dimensions need be (num_signals x num_samples).
 We can do this because AWGN with a phase shift applied is still AWGN.
 """
-noise = np.random.normal(0,np.sqrt(1),(M,N)) + 1j*np.random.normal(0,np.sqrt(1),(M,N))
+noise = np.random.randn(M,N) + 1j*np.random.randn(M,N)
+noise = 0.5 * noise # split between Re and Im components.
 
 # Create received signal
 tx_signal = soi_matrix + noise
 
 # R matrix calculation
 # outside lib methds to allow different ways of calculating and augmenting
-R = np.cov(tx_signal)
+R = (tx_signal @ tx_signal.conj().T)/tx_signal.shape[1]
 
-# Generate steering vectors        
-ula_st_vectors = ula_steering_vectors(M,0.5,num_st_vecs=1000)
+# Generate steering vectors
+ula_st_vectors = ula_scan_steering_matrix(M,0.5,angular_resolution=1)
 
 # DOA estimation
-Bartlett_PAD = BartlettBeamformer.calculate_pwr_ang_dens(R, ula_st_vectors)
-Capon_PAD = CaponBeamformer.calculate_pwr_ang_dens(R, ula_st_vectors)
+estimates,Bartlett_PAD = BartlettBeamformer.estimate_doa(R, ula_st_vectors)
+print("Bartlett_PAD estimates", estimates)
+estimates,Capon_PAD = CaponBeamformer.estimate_doa(R, ula_st_vectors)
+print("Capon_PAD estimates", estimates)
+estimates,MUSIC_ORTAD = MUSIC.estimate_doa(R, ula_st_vectors)
+print("MUSIC_ORTAD estimates", estimates)
 
-DOA_plot([Bartlett_PAD,Capon_PAD], inc_ang_deg, labels=["Bartlett","Capon"])
+DOA_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC"])
+DOA_polar_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC"])
 
 
 # ===============================================================================================
 # Complex case
-# Params: 
+# Params:
 # SNR = 1
 # sig_numb = 3
 
-M = 16
+M = 4
 d = 0.5 # in wavelengths
-N = 10000  # sample size
+N = 100  # sample size
 
 inc_ang_deg = [-40, 20, 30]
-
-thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x P) Incident angles of test signal   
-P = thetas_deg.shape[1] # P MUST BE < M FOR CORRECT DETECTION
+thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x K) Incident angles of test signal
+K = thetas_deg.shape[1] # K MUST BE < M - 1 FOR CORRECT DETECTION
 thetas_rad = np.deg2rad(thetas_deg)
 
-elems = np.arange(M).reshape(-1,1)
-a = np.exp(-1j * 2 * np.pi * d * elems @ np.sin(thetas_rad)) # (M x P)
+# Generate source signals
+soi = np.random.randn(K, N)   # Signal(s) of Interest
 
-# Generate source signal
-soi = np.random.randn(P, N)   # Signal(s) of Interest
-   
-soi_matrix = a @ soi # (M x P) @ (P x N) = (M x N)
+elems = np.arange(M).reshape(-1,1)
+a = np.exp(-1j * 2 * np.pi * d * elems @ np.sin(thetas_rad)) # (M x K)
+
+soi_matrix = a @ soi # (M x K) @ (K x N) = (M x N)
 
 # Generate multichannel uncorrelated noise
 """
@@ -90,7 +128,7 @@ then "noise" var dimensions need be (num_sensors x num_samples).
 
 Noise prior to phase shifting samples:
 For AWGN, noise can be added to received signal prior to phase-shifting and act as environmental noise
-then "noise" var dimensions need be (num_singals x num_samples).
+then "noise" var dimensions need be (num_signals x num_samples).
 We can do this because AWGN with a phase shift applied is still AWGN.
 """
 noise = np.random.normal(0,np.sqrt(1),(M,N)) + 1j*np.random.normal(0,np.sqrt(1),(M,N))
@@ -100,37 +138,36 @@ tx_signal = soi_matrix + noise
 
 # R matrix calculation
 # outside lib methds to allow different ways of calculating and augmenting
-R = np.cov(tx_signal)
+R = (tx_signal @ tx_signal.conj().T)/tx_signal.shape[1]
 
-# Generate steering vectors        
-ula_st_vectors = ula_steering_vectors(M,0.5,num_st_vecs=1000)
+# Generate steering vectors
+ula_st_vectors = ula_scan_steering_matrix(M,0.5,angular_resolution=1)
 
 # DOA estimation
-Bartlett_PAD = BartlettBeamformer.calculate_pwr_ang_dens(R, ula_st_vectors)
-Capon_PAD = CaponBeamformer.calculate_pwr_ang_dens(R, ula_st_vectors)
+Bartlett_PAD = BartlettBeamformer.calculate_spectrum(R, ula_st_vectors)
+Capon_PAD = CaponBeamformer.calculate_spectrum(R, ula_st_vectors)
 DOA_plot([Bartlett_PAD,Capon_PAD], inc_ang_deg, labels=["Bartlett","Capon"])
-
+DOA_polar_plot([Bartlett_PAD,Capon_PAD], inc_ang_deg, labels=["Bartlett","Capon"])
 
 # ===============================================================================================
 # Coherent case
-M = 16
+M = 4
 d = 0.5 # in wavelengths
-N = 10000  # sample size
+N = 100  # sample size
 
-inc_ang_deg = [-40, 20]
+inc_ang_deg = [50, 80]
+thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x K) Incident angles of test signal   
+K = thetas_deg.shape[1] # K MUST BE < M - 1 FOR CORRECT DETECTION
+thetas_rad = np.deg2rad(thetas_deg)
 
 # Generate multiple copies of same signal
 soi = np.random.randn(1, N)   # Signal(s) of Interest
-soi = np.repeat(soi,2,axis=0)
-   
-thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x P) Incident angles of test signal   
-P = thetas_deg.shape[1] # P MUST BE < M FOR CORRECT DETECTION
-thetas_rad = np.deg2rad(thetas_deg)
+soi = np.repeat(soi,K,axis=0)
 
 elems = np.arange(M).reshape(-1,1)
-a = np.exp(-1j * 2 * np.pi * d * elems @ np.sin(thetas_rad)) # (M x P)
+a = np.exp(-1j * 2 * np.pi * d * elems @ np.sin(thetas_rad)) # (M x K)
 
-soi_matrix = a @ soi # (M x P) @ (P x N) = (M x N)
+soi_matrix = a @ soi # (M x K) @ (K x N) = (M x N)
 
 # Generate multichannel uncorrelated noise
 """
@@ -141,7 +178,7 @@ then "noise" var dimensions need be (num_sensors x num_samples).
 
 Noise prior to phase shifting samples:
 For AWGN, noise can be added to received signal prior to phase-shifting and act as environmental noise
-then "noise" var dimensions need be (num_singals x num_samples).
+then "noise" var dimensions need be (num_signals x num_samples).
 We can do this because AWGN with a phase shift applied is still AWGN.
 """
 noise = np.random.normal(0,np.sqrt(1),(M,N)) + 1j*np.random.normal(0,np.sqrt(1),(M,N))
@@ -151,12 +188,13 @@ tx_signal = soi_matrix + noise
 
 # R matrix calculation
 # outside lib methds to allow different ways of calculating and augmenting
-R = np.cov(tx_signal)
+R = (tx_signal @ tx_signal.conj().T)/tx_signal.shape[1]
 
 # Generate steering vectors        
-ula_st_vectors = ula_steering_vectors(M,0.5,num_st_vecs=1000)
+ula_st_vectors = ula_scan_steering_matrix(M,0.5,angular_resolution=1)
 
 # DOA estimation
-Bartlett_PAD = BartlettBeamformer.calculate_pwr_ang_dens(R, ula_st_vectors)
-Capon_PAD = CaponBeamformer.calculate_pwr_ang_dens(R, ula_st_vectors)
+Bartlett_PAD = BartlettBeamformer.calculate_spectrum(R, ula_st_vectors)
+Capon_PAD = CaponBeamformer.calculate_spectrum(R, ula_st_vectors)
 DOA_plot([Bartlett_PAD,Capon_PAD], inc_ang_deg, labels=["Bartlett","Capon"])
+DOA_polar_plot([Bartlett_PAD,Capon_PAD], inc_ang_deg, labels=["Bartlett","Capon"])
