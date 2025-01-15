2f26d0d0087c7c45422ce8c4f1bdc49349c8f07f
Nikolay <nikolay.iliev@aalto.fi>
2025-01-15 12:10:18 +0200

    Spatial smoothing demos to MC trials

--- demos/demo_eleven.py
+++ demos/demo_eleven.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 from utils import *
 import BartlettBeamformer
 import CaponBeamformer
@@ -10,49 +11,79 @@ import ESPRIT
 # ----------------------------------------------
 # 11         | Multipath coherency | 1 source at -20°, reflection at 20°,  SNR_1 is 0dB, SNR_2 is -5dB, AWGN
 
-def demo_eleven(M = 8,d = 0.5,N = 100, figName = None):        
-    inc_ang_deg = [-20]
-    thetas_deg=np.array(inc_ang_deg + [20]).reshape(1,-1)   # (1 x K) Incident angles of test signal
-    K = thetas_deg.shape[1] # K MUST BE < M - 1 FOR CORRECT DETECTION
-    thetas_rad = np.deg2rad(thetas_deg)
+def demo_eleven(M = 8,d = 0.5,N = 100, figName = None):
+    # Incident angles
+    src_ang_deg = [-20]  # Main signal
+    reflection_ang_deg = [20]  # Reflection
+    src_ang_rad = np.deg2rad(np.array(src_ang_deg).reshape(1, -1)) # (1 x K) DOA of main signal
+    reflection_rad = np.deg2rad(np.array(reflection_ang_deg).reshape(1, -1))
+    K = src_ang_rad.shape[1]  # Number of signals
 
-    # Generate source signals
-    soi = np.random.randn(1, N)   # Signal(s) of Interest
-    # Create K=1 copies of signal
-    soi = np.repeat(soi,K,axis=0)
+    Bartlett_PAD_list = []
+    Capon_PAD_list = []
+    MUSIC_ORTAD_list = []
+    rm_estimates_list = []
+    esp_estimates_list = []
+    for t in range(20):
+      # Signal parameters
+      soi = np.random.randn(1, N)  # Main signal
+      power = np.sqrt(10**(0 / 10))  # SNR for main signal in dB
+      soi = power * soi
 
-    # Augment generated signals with the given SNR
-    snr = np.asarray([0,-5]) # (K,)
-    power = np.sqrt(10**(snr / 10)) 
-    power = np.diag(power) # (K x K)
+      # Reflection modeling: delayed, scaled, and phase-shifted version of main signal
+      reflection_gain = np.sqrt(10**(-3/10))  # Reflection SNR: -5 dB
+      reflection_delay = -10  # Delay in samples
+      rand_ph_shift = scipy.stats.truncnorm((-np.pi/2), (np.pi/2), loc=0, scale=1).rvs(size=1)[0]
+      reflection_phase = np.exp(1j * rand_ph_shift)  # Phase shift
 
-    soi = power @ soi # (K x K) @ (K x N)
+      reflection_signal = (
+          reflection_gain
+          * np.roll(soi[0], reflection_delay)
+          * reflection_phase
+      ).reshape(1, -1)  # Ensure shape consistency
 
-    A = ula_steering_matrix(M,d,thetas_rad) # (M x K)
+      # Steering matrix for the main sig
+      A_main = ula_steering_matrix(M, d, src_ang_rad)  # (M x K)
 
-    steered_soi_matrix = A @ soi # (M x K) @ (K x N) = (M x N)
+      # Steering matrix for the reflection
+      A_reflection = ula_steering_matrix(M, d, reflection_rad)
 
-    # Generate multichannel uncorrelated noise
-    noise = np.random.randn(M,N) + 1j*np.random.randn(M,N)
-    noise = 0.5 * noise # split between Re and Im components.
+      # Combined steered signals (main + reflection)
+      steered_soi_matrix = A_main @ soi + A_reflection @ reflection_signal  # (M x N)
 
-    # Create received signal
-    tx_signal = steered_soi_matrix + noise
+      # Add multichannel AWGN noise
+      noise = np.random.randn(M, N) + 1j * np.random.randn(M, N)
+      noise = 0.5 * noise  # Split power between real and imaginary parts
+      tx_signal = steered_soi_matrix + noise  # Received signal
 
-    # R matrix calculation
-    # outside lib methds to allow different ways of calculating and augmenting
-    R = cov(tx_signal)
+      # Covariance matrix and spatial smoothing
+      # R = cov(tx_signal)  # Main covariance matrix
+      R_main = cov(tx_signal)  # Main covariance matrix
+      subarray_length = M - 1  # Length of subarrays for spatial smoothing
+      R_ss = spatial_smoothing(R_main, subarray_length=subarray_length)
 
-    # Generate steering vectors
-    ula_st_vectors = ula_scan_steering_matrix(M,d,angular_resolution=1)
+      # Generate steering vectors for subarray
+      # ula_st_vectors = ula_scan_steering_matrix(M, d, angular_resolution=1)
+      ula_st_vectors = ula_scan_steering_matrix(subarray_length, d, angular_resolution=1)
 
-    # DOA estimation
-    _ ,Bartlett_PAD = BartlettBeamformer.estimate_doa(R, ula_st_vectors, K)
-    _ ,Capon_PAD = CaponBeamformer.estimate_doa(R, ula_st_vectors, K)
-    _ ,MUSIC_ORTAD = MUSIC.estimate_doa(R, ula_st_vectors, K)
-    rm_estimates,_ = RMUSIC.estimate_doa(R, ula_st_vectors, K)
-    esp_estimates,_ = ESPRIT.estimate_doa(R, ula_st_vectors, K)
-    print("ROOT MUSIC estimates", rm_estimates)
-    print("ESPRIT estimates", esp_estimates)
-    DOA_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD],[np.asarray(rm_estimates), np.asarray(esp_estimates)], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC", "ROOT", "ESPRIT"], save_fig=True, fig_name =figName)
-    DOA_polar_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD],[np.asarray(rm_estimates), np.asarray(esp_estimates)], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC", "ROOT", "ESPRIT"], save_fig=True, fig_name =f'{figName}_polar')
+      # DOA estimation
+      _ ,Bartlett_PAD = BartlettBeamformer.estimate_doa(R_ss, ula_st_vectors, K)
+      _ ,Capon_PAD = CaponBeamformer.estimate_doa(R_ss, ula_st_vectors, K)
+      _ ,MUSIC_ORTAD = MUSIC.estimate_doa(R_ss, ula_st_vectors, K)
+      rm_estimates,_ = RMUSIC.estimate_doa(R_ss, ula_st_vectors, K)
+      esp_estimates,_ = ESPRIT.estimate_doa(R_ss, ula_st_vectors, K)
+      Bartlett_PAD_list.append(Bartlett_PAD.reshape(1,-1))
+      Capon_PAD_list.append(Capon_PAD.reshape(1,-1))
+      MUSIC_ORTAD_list.append(MUSIC_ORTAD.reshape(1,-1))
+      rm_estimates_list.append(rm_estimates.reshape(1,-1))
+      esp_estimates_list.append(esp_estimates.reshape(1,-1))
+
+    Bartlett_PAD = np.concatenate( Bartlett_PAD_list, axis=0 ).mean(axis=0)
+    Capon_PAD = np.concatenate( Capon_PAD_list, axis=0 ).mean(axis=0)
+    MUSIC_ORTAD = np.concatenate( MUSIC_ORTAD_list, axis=0 ).mean(axis=0)
+    rm_estimates = np.concatenate( rm_estimates_list, axis=0 ).mean(axis=0)
+    esp_estimates = np.concatenate( esp_estimates_list, axis=0 ).mean(axis=0)
+
+    DOA_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD],[np.asarray(rm_estimates), np.asarray(esp_estimates)], src_ang_deg, labels=["Bartlett","Capon", "MUSIC", "ROOT", "ESPRIT"], save_fig=True, fig_name =figName)
+    DOA_polar_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD],[np.asarray(rm_estimates), np.asarray(esp_estimates)], src_ang_deg, labels=["Bartlett","Capon", "MUSIC", "ROOT", "ESPRIT"], save_fig=True, fig_name =f'{figName}_polar')
+demo_eleven(figName="demo_eleven_ss")
--- demos/demo_seven.py
+++ demos/demo_seven.py
@@ -11,50 +11,68 @@ import ESPRIT
 # 7          | Correlated sources | 2 sources at -20° and 20°, SNR_1 is 15dB, SNR_1 is -15dB, AWGN, highly correlated
 
 def demo_seven(M = 8,d = 0.5,N = 100, figName = None):        
-    inc_ang_deg = [-20, 20]
+    inc_ang_deg = [-50,-30,-10]
     thetas_deg=np.array(inc_ang_deg).reshape(1,-1)   # (1 x K) Incident angles of test signal
     K = thetas_deg.shape[1] # K MUST BE < M - 1 FOR CORRECT DETECTION
     thetas_rad = np.deg2rad(thetas_deg)
-    
-    # Generate source signals
-    soi = np.random.randn(K, N)   # Signal(s) of Interest
 
-    # Augment generated signals with the given SNR
-    snr = np.asarray([15,-15]) # (K,)
-    power = np.sqrt(10**(snr / 10)) 
-    power = np.diag(power) # (K x K)
-    
-    #Correlate using Cholesky
-    correlation_matrix = np.array([[1, 0.75], [0.75, 1]])  # High correlation
-    soi = np.linalg.cholesky(correlation_matrix).dot(soi)
-    
-    soi = power @ soi # (K x K) @ (K x N)
-    
-    A = ula_steering_matrix(M,d,thetas_rad) # (M x K)
-    
-    steered_soi_matrix = A @ soi # (M x K) @ (K x N) = (M x N)
-    
-    # Generate multichannel uncorrelated noise
-    noise = np.random.randn(M,N) + 1j*np.random.randn(M,N)
-    noise = 0.5 * noise # split between Re and Im components.
-    
-    # Create received signal
-    tx_signal = steered_soi_matrix + noise
-    
-    # R matrix calculation
-    # outside lib methds to allow different ways of calculating and augmenting
-    R = cov(tx_signal)
-    
-    # Generate steering vectors
-    ula_st_vectors = ula_scan_steering_matrix(M,d,angular_resolution=1)
-    
-    # DOA estimation
-    _ ,Bartlett_PAD = BartlettBeamformer.estimate_doa(R, ula_st_vectors, K)
-    _ ,Capon_PAD = CaponBeamformer.estimate_doa(R, ula_st_vectors, K)
-    _ ,MUSIC_ORTAD = MUSIC.estimate_doa(R, ula_st_vectors, K)
-    rm_estimates,_ = RMUSIC.estimate_doa(R, ula_st_vectors, K)
-    esp_estimates,_ = ESPRIT.estimate_doa(R, ula_st_vectors, K)
-    print("ROOT MUSIC estimates", rm_estimates)
-    print("ESPRIT estimates", esp_estimates)
+    Bartlett_PAD_list = []
+    Capon_PAD_list = []
+    MUSIC_ORTAD_list = []
+    rm_estimates_list = []
+    esp_estimates_list = []
+    for t in range(20):
+      # Generate source signals
+      soi = np.random.randn(K, N)   # Signal(s) of Interest
+
+      # Augment generated signals with the given SNR
+      snr = np.asarray([0,-5,-10]) # (K,)
+      power = np.sqrt(10**(snr / 10))
+      power = np.diag(power) # (K x K)
+
+      #Correlate using Cholesky
+      correlation_matrix = np.random.uniform(low=0.97, high=0.99, size=(K,K))  # High correlation
+      np.fill_diagonal(correlation_matrix, 1)  # Set diagonal elements to 1
+      soi = np.linalg.cholesky(correlation_matrix).dot(soi)
+
+      soi = power @ soi # (K x K) @ (K x N)
+
+      A = ula_steering_matrix(M,d,thetas_rad) # (M x K)
+
+      steered_soi_matrix = A @ soi # (M x K) @ (K x N) = (M x N)
+
+      # Generate multichannel uncorrelated noise
+      noise = np.random.randn(M,N) + 1j*np.random.randn(M,N)
+      noise = 0.5 * noise # split between Re and Im components.
+
+      # Create received signal
+      tx_signal = steered_soi_matrix + noise
+
+      # R matrix calculation
+      # outside lib methds to allow different ways of calculating and augmenting
+
+      R_main = cov(tx_signal)
+      R_ss = spatial_smoothing(R_main, subarray_length=(M-1))
+      # Generate steering vectors
+      ula_st_vectors = ula_scan_steering_matrix((M-1),d,angular_resolution=1)
+
+      # DOA estimation
+      _ ,Bartlett_PAD = BartlettBeamformer.estimate_doa(R_ss, ula_st_vectors, K)
+      _ ,Capon_PAD = CaponBeamformer.estimate_doa(R_ss, ula_st_vectors, K)
+      _ ,MUSIC_ORTAD = MUSIC.estimate_doa(R_ss, ula_st_vectors, K)
+      rm_estimates,_ = RMUSIC.estimate_doa(R_ss, ula_st_vectors, K)
+      esp_estimates,_ = ESPRIT.estimate_doa(R_ss, ula_st_vectors, K)
+      Bartlett_PAD_list.append(Bartlett_PAD.reshape(1,-1))
+      Capon_PAD_list.append(Capon_PAD.reshape(1,-1))
+      MUSIC_ORTAD_list.append(MUSIC_ORTAD.reshape(1,-1))
+      rm_estimates_list.append(rm_estimates.reshape(1,-1))
+      esp_estimates_list.append(esp_estimates.reshape(1,-1))
+
+    Bartlett_PAD = np.concatenate( Bartlett_PAD_list, axis=0 ).mean(axis=0)
+    Capon_PAD = np.concatenate( Capon_PAD_list, axis=0 ).mean(axis=0)
+    MUSIC_ORTAD = np.concatenate( MUSIC_ORTAD_list, axis=0 ).mean(axis=0)
+    rm_estimates = np.concatenate( rm_estimates_list, axis=0 ).mean(axis=0)
+    esp_estimates = np.concatenate( esp_estimates_list, axis=0 ).mean(axis=0)
+
     DOA_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD],[np.asarray(rm_estimates), np.asarray(esp_estimates)], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC", "ROOT", "ESPRIT"], save_fig=True, fig_name =figName)
     DOA_polar_plot([Bartlett_PAD,Capon_PAD, MUSIC_ORTAD],[np.asarray(rm_estimates), np.asarray(esp_estimates)], inc_ang_deg, labels=["Bartlett","Capon", "MUSIC", "ROOT", "ESPRIT"], save_fig=True, fig_name =f'{figName}_polar')
